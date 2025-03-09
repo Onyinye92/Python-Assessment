a0476c94cea7646caa06b132efa26b455085a7cd
Onyinye  Sarah Azunna <164611688+Onyinye92@users.noreply.github.com>
2025-03-09 20:04:16 +0100

    Add files via upload

---
+++ Basic_Variable_Manipulation.py
@@ -0,0 +1,12 @@
+# Declare variables
+name = "Sarah"
+age = 30
+
+# Print the sentence
+print(f"Hello, my name is {name} and I am {age} years old.")
+
+# Prompt user for their favorite color
+favorite_color = input("Enter your favorite color: ")
+
+# Print the message
+print(f"Your favorite color is {favorite_color}. That’s a great choice!")
